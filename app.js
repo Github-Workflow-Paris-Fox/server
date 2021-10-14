80c2f827dd37e7b2c814d409d3c1feb92b95bca3
Edison Zhou <49446586+edirates@users.noreply.github.com>
2021-10-14 14:01:45 +0700

    Finishing login feature

--- app.js
+++ app.js
@@ -3,4 +3,23 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({
+        where: {
+            email
+        }
+    })
+    .then((user) => {
+        if (user.password === password) {
+            res.status(200).json({ token: "asdfasdfasdf" });
+        } else {
+            res.status(401).json({ message: "invalid email or password" });
+        }
+    })
+    .catch((err) => {
+        res.status(500).json({ message: err.message });
+    });
+});
+
 app.listen(PORT, () => console.log(`ILY ${PORT}`));
