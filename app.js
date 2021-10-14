addd2e5440cf0fffefd4ebefd1ac91ae68365fff
Edison Zhou <49446586+edirates@users.noreply.github.com>
2021-10-14 13:43:13 +0700

    Finishing register feature

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.post("/register", (req, res) => {
+    const { email, password } = req.body;
+    User.create({
+        email,
+        password
+    })
+    .then((user) => {
+        res.status(201).json({ user });
+    })
+    .catch((err) => {
+        res.status(500).json({ message: err.message });
+    });
+});
+
 app.listen(PORT, () => console.log(`ILY ${PORT}`));
